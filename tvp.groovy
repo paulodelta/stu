fa4825b777039eb59fa7a8ce49747af055861bbc
paulodelta <99894880+paulodelta@users.noreply.github.com>
2025-01-14 14:59:42 -0300

    Update tvp.groovy

--- tvp.groovy
+++ tvp.groovy
@@ -7,6 +7,28 @@ pipeline {
                 echo 'Hello from tvp'
             }
         }
+        pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello from tvp'
+            }
+        }
+        stage('Detect Pull Request') {
+            steps {
+                script {
+                    if (env.CHANGE_ID) {
+                        echo "Branch tvp. Este build é de um Pull Request: #${env.CHANGE_ID}"
+                    } else {
+                        echo "Branch tvp. Este build NÃO é de um Pull Request."
+                    }
+                }
+            }
+        }
+    }
+}
     }
 }
 
