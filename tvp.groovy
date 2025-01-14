7963ab9753a0708515cc9974f253921436034bba
paulodelta <99894880+paulodelta@users.noreply.github.com>
2025-01-14 15:47:00 -0300

    Update tvp.groovy

--- tvp.groovy
+++ tvp.groovy
@@ -7,22 +7,13 @@ pipeline {
                 echo 'Hello from tvp'
             }
         }
-        pipeline {
-    agent any
-
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello from tvp'
-            }
-        }
         stage('Detect Pull Request') {
             steps {
                 script {
                     if (env.CHANGE_ID) {
-                        echo "Branch tvp. Este build é de um Pull Request: #${env.CHANGE_ID}"
+                        echo "tvp integration branch. Este build é de um Pull Request: #${env.CHANGE_ID}"
                     } else {
-                        echo "Branch tvp. Este build NÃO é de um Pull Request."
+                        echo "tvp integration branch. Este build NÃO é de um Pull Request."
                     }
                 }
             }
